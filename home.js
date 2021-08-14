a59c67db53a5c35aeeb0bbf92e6457aa591181b3
vaids <vaids@pintclickcare.com>
2021-08-14 13:40:23 -0400

    init repo

---
+++ home.js
@@ -0,0 +1,11 @@
+
+
+/*
+Using js short hand to declare variables
+*/
+let price = 49.99,
+    productName = 'Hiking Boots',
+    discounted = true;
+
+showMessage(productName);
+console.log(productName);
