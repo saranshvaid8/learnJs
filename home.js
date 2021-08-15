40f965710ded533052b4b865d56ab0e68ee0ba0c
vaids <vaids@pintclickcare.com>
2021-08-15 17:29:21 -0400

    updating profile info

--- home.js
+++ home.js
@@ -5,7 +5,7 @@ Using js short hand to declare variables
 */
 let price = 49.99,
     productName = 'Hiking Boots',
+    message = `Welcome to ${productName}`,
     discounted = true;
 
-showMessage(productName);
-console.log(productName); 
+showMessage(message);
