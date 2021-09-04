1596b2b0ecb109ab21c8425bad61b1fb0642840a
vaids <vaids@pintclickcare.com>
2021-09-04 11:56:42 -0400

    using functions in objects

--- home.js
+++ home.js
@@ -8,4 +8,24 @@ let price = 49.99,
     message = `Welcome to ${productName}`,
     discounted = true;
 
-    (price > 100) ? showMessage(message): showMessage("No");
+
+
+function getSecretMessage(value){
+    let code = value * 42;
+    return code;
+}
+
+let person = {
+    name : 'Sunny',
+    age: 31,
+    parTime: false,
+    getMessage: function() {
+        return getSecretMessage(90);
+    }
+};
+//Variable assignment
+let secretCode = getSecretMessage(3);
+let personMessage = person.getMessage();
+    (price < 10) ? showMessage(secretCode) : showMessage(personMessage)
+    
+    
